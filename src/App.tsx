09af632b61583c7a768ed17b3117cf5be4b79eec
chenyuhao <cccchenyuhao@gmail.com>
2023-05-28 10:32:38 +0800

    readme

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { useEffect, useState } from "react";
 import img from "./assets/img.png";
+import { add } from "lodash-es";
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div>
       app
-      <img src={img} />
+      <img src={img} width={300} />
+      <div>{add(1, 2)}</div>
     </div>
   );
 }
