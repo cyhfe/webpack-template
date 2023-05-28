5262fa7fca50aa63611184c85a38a3cb59bf6398
chenyuhao <cccchenyuhao@gmail.com>
2023-05-28 12:20:43 +0800

    emotion

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import img from "./assets/img.png";
 import { add } from "lodash-es";
+import { css } from "@emotion/react";
 function App() {
   const [count, setCount] = useState(0);
 
@@ -11,7 +12,13 @@ function App() {
     <div>
       app
       <img src={img} width={300} alt="img" />
-      <div>{add(1, 2)}</div>
+      <div
+        css={css`
+          color: red;
+        `}
+      >
+        {add(1, 2)}
+      </div>
       <button onClick={() => setCount((c) => c + 1)}>{count}</button>
     </div>
   );
